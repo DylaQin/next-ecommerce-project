cc5ca2b316b791c17331b3bfd7f05c04b14cbe2a
DylaQin <dyla1024@outlook.com>
2023-12-29 23:16:33 +0000

    initial commit

---
+++ ecommerce-admin/pages/index.js
@@ -0,0 +1,36 @@
+import Layout from "@/components/Layout";
+import {useSession} from "next-auth/react";
+
+export default function Home() {
+  const {data: session} = useSession();
+  return <Layout>
+    <div className="text-blue-900 flex justify-between">
+      <h2>
+        Hello, <b>{session?.user?.name}</b>
+      </h2>
+      <div className="flex bg-gray-300 gap-1 text-black rounded-lg overflow-hidden">
+        <img src={session?.user?.image} alt="" className="w-6 h-6"/>
+        <span className="px-2">
+          {session?.user?.name}
+        </span>
+      </div>
+    </div>
+  </Layout>
+}
+
+// original layout
+// import { useSession, signIn, signOut } from "next-auth/react"
+
+// export default function Component() {
+//   const { data: session } = useSession()
+//   if(session) {
+//     return <>
+//       Signed in as {session.user.email} <br/>
+//       <button onClick={() => signOut()}>Sign out</button>
+//     </>
+//   }
+//   return <>
+//     Not signed in <br/>
+//     <button onClick={() => signIn()}>Sign in</button>
+//   </>
+// }
